c31365ca0a78e32213a8dc2b8e9f0d7f23f052d6
user.email <jobayer.ahmed109@gmail.com>
2024-01-12 21:27:58 +0600

    Todo app initial setup done

---
+++ src/components/todos/index.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Todo extends Component {
+  render() {
+    return (
+      <div>
+        <h1 className="display-5 fw-semibold text-center mb-5">Smart Todo</h1>
+      </div>
+    );
+  }
+}
+
+export default Todo;
